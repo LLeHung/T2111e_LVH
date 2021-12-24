f24c3b228175d159905fbea0ef7ab1374effb895
Le Hung <ktr.lehung@gmail.com>
2021-12-24 17:22:10 +0700

    lab s3

---
+++ lab s3/laps3.1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	if (2<=a<=8){
+		if (a==2);
+		printf("day la thu 2");
+		else if(a==3);
+		printf("day la thu 3");
+		else if(a==4);
+		printf("day la thu 4");
+		else if(a==5);
+		printf("day la thu 5");
+		else if(a==6);
+		printf("day la thu 6");
+		else if(a==7);
+		printf("day la thu 7");
+		else if(a==8);
+		printf("day la chu nhat");
+	}else{
+		printf(khong phai ngay trong tuan);
+	}
+}
